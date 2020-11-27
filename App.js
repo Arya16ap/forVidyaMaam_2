4cb2192be9784830630492d41beec8fd0da6e7bc
Arya16ap <58871255+Arya16ap@users.noreply.github.com>
2020-11-27 18:59:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/loginScreen'
+import HomeScreen from './screens/homescreen';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation';
+export default function App() {
+  return (
+    <AppContainer/>
+
+  );
+}
+
+const switchNavigator = createSwitchNavigator({
+  LoginScreen:{screen:LoginScreen},
+  HomeScreen:{screen:HomeScreen}
+
+})
+const AppContainer = createAppContainer(switchNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/homescreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import{Text,View,StyleSheet,TouchableOpacity,TextInput,Modal,KeyboardAvoidingView,ScrollView}from'react-native';
+
+
+export default class HomeScreen extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>hommme HomeScreen</Text>
+            </View>
+        )
+    }
+
+}
